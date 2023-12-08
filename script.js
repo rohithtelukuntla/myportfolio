b038ceb4cc8a0c4a1182763ed68c5d78e158bca4
rohithtelukuntla <telukuntlarohith1995@gmail.com>
2023-12-08 15:47:02 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+//rotate text js code
+// let text = document.querySelector(".text p");
+// text.innerHTML = text.innerHTML.split("").map((char, i)=>
+//    `<b style=transform:rotate(${i * 6.3}deg*)>${char}</b>`
+// ).join("");
